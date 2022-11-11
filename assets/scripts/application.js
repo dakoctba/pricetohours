9f283691a5c2e2ce4dc9b27d5c75dc84b8659dbf
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2022-11-11 11:52:34 -0300

    ok version

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -3,7 +3,17 @@
 // Em pt-br
 // onkeypress="return (event.charCode >= 48 && event.charCode <= 57) ||  event.charCode == 44 || event.charCode == 0"
 
-console.log("ok");
+function calc(event) {
+    let _cost = document.getElementById('cost').value;
+    let _wage = document.getElementById('wage').value;
+
+    _cost = _cost.replace(/\D/g, "");
+    _wage = _wage.replace(/\D/g, "");
+
+    console.log('_cost ~> '+ _cost);
+    console.log('_wage ~> '+ _wage);
+    console.log(_cost / _wage + ' horas');
+}
 
 function cost(event) {
     if ((event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46 || event.charCode == 0) {
